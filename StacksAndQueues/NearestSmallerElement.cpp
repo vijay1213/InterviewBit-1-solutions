5d1ae305ecd4cf2024e229be4564f01c82dbe23c
Vijay Chathe <76934330+vijay1213@users.noreply.github.com>
2022-06-28 11:42:42 +0530

    Update NearestSmallerElement.cpp

--- StacksAndQueues/NearestSmallerElement.cpp
+++ StacksAndQueues/NearestSmallerElement.cpp
@@ -56,3 +56,22 @@ vector<int> Solution::prevSmaller(vector<int> &A) {
     }
     return corresEle;
 }
+
+vector<int> Solution::prevSmaller(vector<int> &A) {
+    vector<int> ans;
+    ans.resize(A.size());
+
+    stack<int> st;
+    
+    for (int i = 0; i < A.size(); i++) {
+        while (!st.empty() && st.top() >= A[i]) st.pop();
+        if (st.empty()) {
+            ans[i] = -1;
+        } else {
+            ans[i] = st.top();
+        }
+        st.push(A[i]);
+    }
+    return ans;
+}
+
